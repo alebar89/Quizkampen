74830ea39fd7781fafef489aeeca2949c6e65e9c
Ali Abdullah <aliila-3@hotmail.com>
2023-11-21 17:08:43 +0100

    Multiuser

---
+++ src/Quizkampen/QuizkampenClient.java
@@ -0,0 +1,32 @@
+package Quizkampen;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class QuizkampenClient {
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 5000);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+
+                String serverResponse = in.readLine();
+                System.out.println("Server: " + serverResponse);
+
+                if (serverResponse.equals("Quizet är slut.")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
