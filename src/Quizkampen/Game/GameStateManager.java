9092969b31e9442ebf48cbb2ded1f1b18023ebdc
Alexa <alexander.pino@nackademin.yh.antagning.se>
2023-11-25 14:30:49 +0100

    La till GameStateManager för att kunna hantera olika paneler beroende på stadie i spelet.

---
+++ src/Quizkampen/Game/GameStateManager.java
@@ -0,0 +1,49 @@
+package Quizkampen.Game;
+
+import Quizkampen.GamePanels.LoginPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameStateManager {
+
+    private CardLayout cardLayout;  // använder cardlayout för att skifta mellan de olika rutorna.
+    // det hela går ut på att alla paneler ligger på varandra men man väljer ut den panel som ska visas vid respektive stadie.
+    private JPanel cardPanel;
+
+    public static final int LOGIN_STATE = 0;
+    public static final int LOBBY_STATE = 1;
+    public static final int CATEGORY_STATE = 2;
+    public static final int GAME_STATE = 3;
+    public static final int RESULT_STATE = 4;
+
+    public static int CURRENT_STATE = LOGIN_STATE;
+
+    public GameStateManager(JFrame frame) {
+        cardPanel = new JPanel(new CardLayout());
+        cardLayout = (CardLayout)cardPanel.getLayout(); // kastar layouten till CardLayout så vi kan använda metoderna för det objektet
+        frame.add(cardPanel);
+
+        intitializeStates();
+
+    }
+
+    // initierar de olika korten(panelerna) som ska displayas för de olika stadierna i spelet.
+    // lägg till era paneler här så kan vi fixa logiken till att hoppa till varje state.
+    private void intitializeStates() {
+        cardPanel.add(new LoginPanel(this), String.valueOf(LOGIN_STATE));
+
+
+    }
+
+
+    // metod för att byta stadieruta.
+    public void setState(int state) {
+        String stateString = String.valueOf(state);
+        cardLayout.show(cardPanel, stateString);
+        CURRENT_STATE = state;
+    }
+
+
+
+}
---
+++ src/Quizkampen/GamePanels/LoginPanel.java
@@ -0,0 +1,26 @@
+package Quizkampen.GamePanels;
+
+import Quizkampen.Game.GameStateManager;
+
+import javax.swing.*;
+public class LoginPanel extends JPanel {
+
+    public LoginPanel(GameStateManager gsm) {
+
+        setLayout(null);
+
+        JLabel label = new JLabel("USERNAME:");
+        label.setBounds(50, 165, 100,20);
+        add(label);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(120,165,150, 25);
+        add(userText);
+
+        JButton buttonLogin = new JButton("Login");
+        buttonLogin.setBounds(50, 215, 275,25);
+        add(buttonLogin);
+
+        buttonLogin.addActionListener(e -> gsm.setState(GameStateManager.LOBBY_STATE));
+    }
+}
